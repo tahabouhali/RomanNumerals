022f48a3038962bd81426b1140573ddeb8c5e9f6
Taha BOUHALI <32977876+tahabouhali@users.noreply.github.com>
2021-02-24 21:27:09 +0100

    Update RomanNumerals.java

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -13,6 +13,12 @@ public class RomanNumerals {
         X(10),
         XL(40),
         L(50),
+        XC(90),
+        C(100),
+        CD(400),
+        D(500),
+        CM(900),
+        M(1000)
         ;
         int value;
         romanNum(int value) {
